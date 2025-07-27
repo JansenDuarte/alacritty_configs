089c714a108ae53fab91d675d6231cb642cd0b14
jansenduarte1 <jansenduarte.dev@gmail.com>
2025-07-27 12:16:36 -0300

    Change keybindings

--- init.ps1
+++ init.ps1
@@ -1,1 +1,14 @@
-Write-Host "Alacritty Terminal`n"
+#Write-Host ":----------------:`n" -ForegroundColor Gray
+#Write-Host ":----------------:`n" -ForegroundColor DarkGray
+#Write-Host ":----------------:`n" -ForegroundColor Blue
+#Write-Host ":----------------:`n" -ForegroundColor Green
+#Write-Host ":----------------:`n" -ForegroundColor Cyan
+#Write-Host ":----------------:`n" -ForegroundColor Red
+#Write-Host ":----------------:`n" -ForegroundColor Magenta
+#Write-Host ":----------------:`n" -ForegroundColor Yellow
+Write-Host "|------------------|" -ForegroundColor Yellow
+Write-Host "|" -NoNewline  -ForegroundColor Yellow
+Write-Host "Alacritty Terminal" -NoNewline -ForegroundColor Cyan
+Write-Host "|" -ForegroundColor Yellow
+Write-Host "|------------------|`n" -ForegroundColor Yellow
+
