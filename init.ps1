6b6c85037486f87e8ccf0e0f8500f1b720f2bd86
jansenduarte1 <jansenduarte.dev@gmail.com>
2025-07-31 17:23:21 -0300

    Fix shortcut & simple startup script

--- init.ps1
+++ init.ps1
@@ -1,14 +1,41 @@
-#Write-Host ":----------------:`n" -ForegroundColor Gray
-#Write-Host ":----------------:`n" -ForegroundColor DarkGray
-#Write-Host ":----------------:`n" -ForegroundColor Blue
-#Write-Host ":----------------:`n" -ForegroundColor Green
-#Write-Host ":----------------:`n" -ForegroundColor Cyan
-#Write-Host ":----------------:`n" -ForegroundColor Red
-#Write-Host ":----------------:`n" -ForegroundColor Magenta
-#Write-Host ":----------------:`n" -ForegroundColor Yellow
-Write-Host "|------------------|" -ForegroundColor Yellow
-Write-Host "|" -NoNewline  -ForegroundColor Yellow
-Write-Host "Alacritty Terminal" -NoNewline -ForegroundColor Cyan
-Write-Host "|" -ForegroundColor Yellow
-Write-Host "|------------------|`n" -ForegroundColor Yellow
+
+#for($i = 0; $i -le 10; $i++)
+#{
+#	if ($i % 2 -eq 0)
+#	{
+#		Write-Host "0"
+#	}
+#	else
+#	{
+#		Write-Host "1"
+#	}
+#}
+
+
+$terminalName = ' Alacritty Terminal '
+
+
+
+
+$size = $terminalName.Length + 2
+$tab = ("-" * $size)
+$space = (" " * $size)
+
+
+
+
+Clear-Host
+
+Write-Host "|$tab|" -ForegroundColor Yellow
+Write-Host "|$space|" -ForegroundColor Yellow
+
+Write-Host "|>" -NoNewline  -ForegroundColor Yellow
+Write-Host $terminalName -NoNewline -ForegroundColor Cyan
+Write-Host "<|" -ForegroundColor Yellow
+
+Write-Host "|$space|" -ForegroundColor Yellow
+Write-Host "|$tab|" -ForegroundColor Yellow
+
+#just a new line
+Write-Host ""
 
